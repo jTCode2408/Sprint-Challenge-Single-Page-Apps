755ee96b40a256664931606d8ac4ae531aad0ab1
Jasmine Terry <jasmine.terry91@gmail.com>
2019-12-20 15:18:57 -0500

    final push w/ styling added

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import {Link} from "react-router-dom";
-import CharacterCard from './CharacterCard';
+ import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, Toast, ToastBody} from "reactstrap";
 const CharacterList = props => {
   // TODO: Add useState to track data from useEffect
 const [characters, setCharacters] = useState([])
+
  useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
@@ -27,9 +28,13 @@ const [characters, setCharacters] = useState([])
 
   return (
     <section className="character-list">
-      <div className = "characters-title">
-    <h1>Character's List</h1>
-    </div>
+       <div className="p-3 bg-primary my-2 rounded">
+        <Toast>
+          <ToastBody>
+          Character's List
+          </ToastBody>
+        </Toast>
+      </div>
       {characters.map(character => (
         <Link to = {`${character.id}`}>
 <CharacterInfo key = {character.id} character = {character}/>
@@ -45,18 +50,21 @@ const [characters, setCharacters] = useState([])
 function CharacterInfo ({ character }) {
   const {name, status, species, gender, image, origin} = character;
   return (
-    
-    <Link to = {`${character.id}`}
-    key= {character.id}>
-     <h3> Name:{name}</h3>
-     <img src = {image}/>
-     <p>  Dead or Alive?{status}</p>
-    <p>  Species: {species}</p>
-    <p>  Gender: {gender}</p>
-  <p>Home World: {origin.name}</p>
+    <div>
+      <Card>
+        <CardImg top width="100%" src= {image}/>
+        <CardBody>
+          <CardTitle>Name:{name}</CardTitle>
+          <CardSubtitle>Dead or Alive?{status}</CardSubtitle>
+          <CardSubtitle> Species: {species}</CardSubtitle>
+          <CardSubtitle>  Gender: {gender}</CardSubtitle>
+          <CardText>Home World: {origin.name}</CardText>
+          <Button> <Link to = {`${character.name}`}
+    key= {character.id}>Search!</Link></Button>
+        </CardBody>
+      </Card>
+    </div>
    
-
-</Link>
 
   );
 }
