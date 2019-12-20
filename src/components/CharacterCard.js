0659d0cbcf9f9678467b247e8a78bf5de124cd48
Jasmine Terry <jasmine.terry91@gmail.com>
2019-12-20 12:10:32 -0500

    axios data returning

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,27 @@
 import React from "react";
+import {useRouteMatch, Link, Rotes, useParams} from "react-router-dom";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+//get id/name of characters 
+//set path for links?
+//
+const CharacterCard = props => {
+
+const {id} = useParams();
+console.log(id);
+const {path, url} = useRouteMatch();
+
+
+  return (
+    <div>
+{/* <Link to = {`${url}/movies/movie`}>
+      Movies
+    </Link>
+    <Route path = {`${path}/movies/movie/:id`}>
+    </Route> */}
+
+    </div>
+  );
 }
+
+
+export default CharacterCard;
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,55 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {Link} from "react-router-dom";
 
-export default function CharacterList() {
+const CharacterList = props => {
   // TODO: Add useState to track data from useEffect
-
-  useEffect(() => {
+const [characters, setCharacters] = useState([])
+ useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    // const getCharacter = () =>{
+      axios
+      .get('https://rickandmortyapi.com/api/character')
+      .then(response => {
+        console.log(response.data.results);
+        setCharacters(response.data);
+      })
+      .catch (error => {
+        console.log('Sever Error', error);
+      });
+    // }
+      // getCharacter
+
+    }, []);
+
+
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(character => (
+        <Link to = {`${character.id}`}>
+<CharacterInfo key = {character.id} character = {character}/>
+
+        </Link>
+      ))}
+      
     </section>
   );
+      }
+
+function CharacterInfo ({ character }) {
+  const {props} = character;
+  return (
+    <Link to = {`${character.id}`}
+    key= {character.id}>
+      Name{props.name}
+      Status{props.status}
+      Species {props.species}
+      Gender {props.gender}
+
+</Link>
+  );
 }
+
+export default CharacterList;
